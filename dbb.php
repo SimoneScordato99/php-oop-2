781afb3f33f40bb7d88314c9977ef1e6d861081c
SimoneScordato99 <scordato.simone@yahoo.com>
2023-06-05 15:59:21 +0200

    add colore.php con traits

--- dbb.php
+++ dbb.php
@@ -5,8 +5,8 @@ require_once __DIR__ . '/models/canGat.php';
 
 
 $arrayProdotti = [
-    $cucciaRossa = new CanGatto('cucciaRossa', '19.99$', 'CAnnosi', new Tipo('cuccia'), 'cane'),
-    $guinzaglioBlu = new CanGatto('guinzaglioBlu', '20.50$', 'Gattini coccolosi s.r.l', new Tipo('guinzaglio'), 'gatto'),
-    $croccGatto = new CanGatto('Croccantini per gatto', '50.50$', 'Gattini coccolosi s.r.l', new Tipo('cibo'), 'gatto'),
+    $cucciaRossa = new CanGatto('cucciaRossa', '19.99$', 'CAnnosi', new Tipo('cuccia'), 'cane', 'rosso'),
+    $guinzaglioBlu = new CanGatto('guinzaglioBlu', '20.50$', 'Gattini coccolosi s.r.l', new Tipo('guinzaglio'), 'gatto', 'blu'),
+    $croccGatto = new CanGatto('Croccantini per gatto', '50.50$', 'Gattini coccolosi s.r.l', new Tipo('cibo'), 'gatto', ''),
     
 ];
